90565000459700e64722e4d2aab78a602fd6d034
saworz <michal.czechowicz7@gmail.com>
2023-11-07 23:07:47 +0100

    Added orders from imported list

---
+++ backend/data/orders.js
@@ -0,0 +1,34 @@
+export let orders = [{
+  id: "b0f17cc5-8b40-4ca5-9142-b61fe3d98c85",
+  image: "images/products/women-stretch-popover-hoodie-black.jpg",
+  name: "Women's Stretch Popover Hoodie",
+  rating: {
+    stars: 4.5,
+    count: 2465
+  },
+  priceCents: 1374,
+  keywords: [
+    "hooded",
+    "hoodies",
+    "sweaters",
+    "womens",
+    "apparel"
+  ],
+},
+{
+  id: "a93a101d-79ef-4cf3-a6cf-6dbe532a1b4a",
+  image: "images/products/bathroom-rug.jpg",
+  name: "Bathroom Bath Rug Mat 20 x 31 Inch - Grey",
+  rating: {
+    stars: 4.5,
+    count: 119
+  },
+  priceCents: 1250,
+  keywords: [
+    "bathmat",
+    "bathroom",
+    "home"
+  ]
+}];
+
+export default orders;
---
+++ frontend/scripts/orders.js
@@ -0,0 +1,45 @@
+import orders from "../../backend/data/orders.js";
+
+let ordersHtml = '';
+
+orders.forEach((order) => {
+  const html = `          
+  <div class="order-item">
+    <div class="order-image-div">
+      <img class="order-image" src=${order.image}>
+    </div>
+    <div class="order-description">
+      <div class="description-title">
+        ${order.name}
+      </div>
+      <div class="description-arrival">
+        Arriving on: November 9
+      </div>
+      <div class="description-quantity">
+        Quantity: 2
+      </div>
+      <button class="buy-again-button button-primary">
+        <img class="buy-again-icon" src="images/icons/buy-again.png">
+        <span class="buy-again-message">Buy it again</span>
+      </button>
+      <div class="product-actions-mobile">
+        <a href="tracking.html">
+          <button class="track-package-button button-secondary">
+            Track package
+          </button>
+        </a>
+      </div>
+    </div>
+    <div class="product-actions">
+      <a href="tracking.html">
+        <button class="track-package-button button-secondary">
+          Track package
+        </button>
+      </a>
+    </div>
+  </div>`;
+
+  ordersHtml += html;
+});
+
+document.querySelector('.js-orders').innerHTML = ordersHtml;
