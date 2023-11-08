d5b25cad5db7ac4a294cc6d483c88fe40f5cfb75
saworz <michal.czechowicz7@gmail.com>
2023-11-08 13:56:03 +0100

    Added empty cart handling in checkout

--- frontend/scripts/checkout.js
+++ frontend/scripts/checkout.js
@@ -7,86 +7,100 @@ const today = dayjs();
 
 function drawCheckoutItems() {
   const checkoutItemsHtml = [];
-  cart.forEach((item) => {
-
-    const html = `<div class="cart-item-container js-cart-item-container-${item.product.id}">
-      <div class="delivery-date js-delivery-date" data-product-id=${item.product.id}>
-        Delivery date: Wednesday, June 15
-      </div>
-  
-      <div class="cart-item-details-grid">
-        <img class="product-image"
-          src="${item.product.image}">
-  
-        <div class="cart-item-details">
-          <div class="product-name">
-            ${item.product.name}
-          </div>
-          <div class="product-price">
-            $${(item.product.priceCents/100).toFixed(2)}
-          </div>
-          <div class="product-quantity">
-            <span>
-              Quantity: <span class="quantity-label">${item.quantity}</span>
-            </span>
-            <span class="update-quantity-link link-primary">
-              Update
-            </span>
-            <span class="delete-quantity-link link-primary js-delete-item" data-product-id=${item.product.id}>
-              Delete
-            </span>
-          </div>
-        </div>
-  
-        <div class="delivery-options">
-          <div class="delivery-options-title">
-            Choose a delivery option:
-          </div>
-  
-          <div class="delivery-option">
-            <input type="radio" class="delivery-option-input"
-              name="delivery-option-${item.product.id}">
-            <div>
-              <div class="delivery-option-date">
-                ${today.add(7, 'days').format('dddd, D, MMMM')}
-              </div>
-              <div class="delivery-option-price">
-                FREE Shipping
-              </div>
-            </div>
-          </div>
-          <div class="delivery-option">
-            <input type="radio" checked class="delivery-option-input"
-              name="delivery-option-${item.product.id}">
-            <div>
-              <div class="delivery-option-date">
-              ${today.add(3, 'days').format('dddd, D, MMMM')}
-              </div>
-              <div class="delivery-option-price">
-                $4.99 - Shipping
-              </div>
-            </div>
-          </div>
-          <div class="delivery-option">
-            <input type="radio" class="delivery-option-input"
-              name="delivery-option-${item.product.id}">
-            <div>
-              <div class="delivery-option-date">
-              ${today.add(1, 'days').format('dddd, D, MMMM')}
-              </div>
-              <div class="delivery-option-price">
-                $9.99 - Shipping
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+  if (cart.length === 0) {
+    const html = `
+    <div class="cart-item-container">
+      Your cart is empty.
+      <button class="back-to-store-button button-primary">
+        Go back to store
+      </button>
     </div>`
-  
-    checkoutItemsHtml.push(html)
-  });
+
+    document.querySelector('.js-order-summary').innerHTML = html;
+  } 
+  else {
+    cart.forEach((item) => {
+
+      const html = `<div class="cart-item-container js-cart-item-container-${item.product.id}">
+        <div class="delivery-date js-delivery-date" data-product-id=${item.product.id}>
+          Delivery date: Wednesday, June 15
+        </div>
+    
+        <div class="cart-item-details-grid">
+          <img class="product-image"
+            src="${item.product.image}">
+    
+          <div class="cart-item-details">
+            <div class="product-name">
+              ${item.product.name}
+            </div>
+            <div class="product-price">
+              $${(item.product.priceCents/100).toFixed(2)}
+            </div>
+            <div class="product-quantity">
+              <span>
+                Quantity: <span class="quantity-label">${item.quantity}</span>
+              </span>
+              <span class="update-quantity-link link-primary">
+                Update
+              </span>
+              <span class="delete-quantity-link link-primary js-delete-item" data-product-id=${item.product.id}>
+                Delete
+              </span>
+            </div>
+          </div>
+    
+          <div class="delivery-options">
+            <div class="delivery-options-title">
+              Choose a delivery option:
+            </div>
+    
+            <div class="delivery-option">
+              <input type="radio" class="delivery-option-input"
+                name="delivery-option-${item.product.id}">
+              <div>
+                <div class="delivery-option-date">
+                  ${today.add(7, 'days').format('dddd, D, MMMM')}
+                </div>
+                <div class="delivery-option-price">
+                  FREE Shipping
+                </div>
+              </div>
+            </div>
+            <div class="delivery-option">
+              <input type="radio" checked class="delivery-option-input"
+                name="delivery-option-${item.product.id}">
+              <div>
+                <div class="delivery-option-date">
+                ${today.add(3, 'days').format('dddd, D, MMMM')}
+                </div>
+                <div class="delivery-option-price">
+                  $4.99 - Shipping
+                </div>
+              </div>
+            </div>
+            <div class="delivery-option">
+              <input type="radio" class="delivery-option-input"
+                name="delivery-option-${item.product.id}">
+              <div>
+                <div class="delivery-option-date">
+                ${today.add(1, 'days').format('dddd, D, MMMM')}
+                </div>
+                <div class="delivery-option-price">
+                  $9.99 - Shipping
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>`
+    
+      checkoutItemsHtml.push(html)
+    });
   
   document.querySelector('.js-order-summary').innerHTML = checkoutItemsHtml.join("");
+  }
+
 };
 
 
