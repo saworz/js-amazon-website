dc779fd49e46675050b7693c67078fc02268bc3f
saworz <michal.czechowicz7@gmail.com>
2023-11-11 18:54:44 +0100

    Cleaned tracking.js code

--- frontend/scripts/tracking.js
+++ frontend/scripts/tracking.js
@@ -1,8 +1,48 @@
 import orders from "../../backend/data/ordersList.js"
 import setFavicon from "./favicon.js";
 
+const getTrackedOrderHtml = (item) => {
+  const html = `
+  <div class="order-tracking">
+    <a class="back-to-orders-link link-primary" href="orders.html">
+      View all orders
+    </a>
 
-const drawOrders = () => {
+    <div class="delivery-date">
+      Arriving on ${item.arrival}
+    </div>
+
+    <div class="product-info">
+      ${item.product.name}
+    </div>
+
+    <div class="product-info">
+      Quantity: ${item.quantity}
+    </div>
+
+    <img class="product-image" src=${item.product.image}>
+
+    <div class="progress-labels-container">
+      <div class="progress-label">
+        Preparing
+      </div>
+      <div class="progress-label current-status">
+        Shipped
+      </div>
+      <div class="progress-label">
+        Delivered
+      </div>
+    </div>
+
+    <div class="progress-bar-container">
+      <div class="progress-bar"></div>
+    </div>
+  </div>`
+
+  return html;
+};
+
+const drawTrackedOrder = () => {
   const urlParams = new URLSearchParams(window.location.search);
   const itemId = urlParams.get('itemId');
   let item;
@@ -11,51 +51,13 @@ const drawOrders = () => {
     order.products.forEach((productObj) => {
       const product = productObj.product
       if (product.id === itemId) {
-        item = product;
+        item = productObj;
       };
     });
-      
-    const html = `
-      <div class="order-tracking">
-        <a class="back-to-orders-link link-primary" href="orders.html">
-          View all orders
-        </a>
-    
-        <div class="delivery-date">
-          Arriving on ${item.arrival}
-        </div>
-    
-        <div class="product-info">
-          ${item.name}
-        </div>
-    
-        <div class="product-info">
-          Quantity: ${item.quantity}
-        </div>
-    
-        <img class="product-image" src=${item.image}>
-    
-        <div class="progress-labels-container">
-          <div class="progress-label">
-            Preparing
-          </div>
-          <div class="progress-label current-status">
-            Shipped
-          </div>
-          <div class="progress-label">
-            Delivered
-          </div>
-        </div>
-    
-        <div class="progress-bar-container">
-          <div class="progress-bar"></div>
-        </div>
-      </div>`
-    
-    document.querySelector('.js-tracking').innerHTML = html;
-    });
+    document.querySelector('.js-tracking').innerHTML = getTrackedOrderHtml(item);
+  });
 
 };
 
 setFavicon();
-drawOrders();
+drawTrackedOrder();
